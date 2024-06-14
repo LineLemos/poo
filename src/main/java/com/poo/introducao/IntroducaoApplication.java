9a53bd5901eebbb0fc0f5ef027503d9c774b9df4
Aline Lemos <alinelemos@id.uff.br>
2024-06-14 19:34:10 -0300

    Projeto Java Inicial

---
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -0,0 +1,22 @@
+package com.poo.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoApplication.class, args);
+
+		System.out.println("Hello, world!");
+
+		int numero1 = 1;
+		double numeroReal;
+		char caractere = 'a';
+		String nome = "Arthur";
+		boolean possoCorrer = true;
+		
+	}
+
+}
